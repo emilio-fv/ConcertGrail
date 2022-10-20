0e30dbab54d624eedad2f257c634213b4234cc0a
Emilio Vazquez <109986999+emilio-fv@users.noreply.github.com>
2022-10-19 19:25:59 -0700

    Revert "Feature/create view listings"

--- ConcertGrail/Controllers/ListingsController.cs
+++ ConcertGrail/Controllers/ListingsController.cs
@@ -1,54 +1,27 @@
-using ConcertGrail.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 namespace ConcertGrail.Controllers;        
 
 public class ListingsController : Controller
 {
-    private MyContext Database; // Instantiate database 
+    // private MyContext _context;
 
-    public ListingsController(MyContext database)
-    {
-        Database = database;
-    }
-
-    private int? userId // Checks if user logged in
-    {
-        get
-        {
-            return HttpContext.Session.GetInt32("UserId");
-        }
-    }
-
-    private bool loggedIn
-    {
-        get
-        {
-            return userId != null;
-        }
-    }
+    // public ListingsController(MyContext context)
+    // {
+    //     _context = context;
+    // }
 
     [HttpGet("/listings/new")] // Route: New Listing Form
-    public IActionResult NewListing()
+    public IActionResult New()
     {
-        if(!loggedIn)
-        {
-            return RedirectToAction("Index", "Users");
-        }
+        // TODO: Add any necessary logic
         return View("NewListing");
     }
 
     [HttpPost("/listings/create")] // Route: Create Listing 
-    public IActionResult Create(Listing newListing)
+    public IActionResult Create()
     {
-        if(!ModelState.IsValid) // Validate form data
-        {
-            return NewListing();
-        }
-
-        newListing.UserId = (int)userId; // Assign user Id 
-        Database.Add(newListing); // Add to db
-        Database.SaveChanges(); // Save changes
+        // TODO: Add any necessary logic
         return RedirectToAction("Share", "Users"); // Redirect to Share page
     }
 }
